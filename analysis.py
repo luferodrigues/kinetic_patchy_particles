6a0bc4a7c72a5eeddfec371734d85634a49289d2
luferodrigues <lfc.rodrigues1994@gmail.com>
2026-08-11 23:51:38 -0300

    Extra functionalities

--- analysis.py
+++ analysis.py
@@ -2,6 +2,104 @@ import numpy as np
 from scipy.spatial import Delaunay
 
 # ----------------------------------------
+# ---------AUXILIARY FUNCTIONS------------
+# ----------------------------------------
+
+# "Correct" numpy histogram: placing distribution between edges
+def correct_histogram(histogram):
+    bins_centers = histogram[1][:-1] + (histogram[1][1] - histogram[1][0]) / 2 # Exclude last point and add half REGULAR bin width
+    return [bins_centers, histogram[0]]
+
+
+
+# ----------------------------------------
+# -------IMPORT CLUSTER FEATURES----------
+# ----------------------------------------
+
+def import_cluster_n_max(file, frame_start = 1, frame_end = 100):
+    feats = np.genfromtxt(file, dtype = int)
+    return feats[frame_start-1:frame_end+1]
+    
+def import_cluster_agg(file, frame_start = 1, frame_end = 100, first_frame = 1, stride = 1):
+    cluster_agg = []
+    current_frame = first_frame
+    with open(file) as f:
+        while True:
+            if current_frame > frame_end:
+                break # Stop reading after end of specified frames range
+            if current_frame < frame_start:
+                f.readline() # Simply skip and not parse the lines with frame < frame_start
+                current_frame += stride
+                continue
+            # Reached the desired frame
+            for i in range(frame_end - frame_start):
+                line = f.readline()
+                split_line = line.split(',')[:-1]
+                agg_frame = list(map(int, split_line))
+                cluster_agg.append(agg_frame)
+                current_frame += stride
+    return cluster_agg
+
+# Generates aggregation histograms for a list of lists with agggregation numbers of different frames
+def generate_agg_histograms(agg_data, max_val, bin_width=2):
+    bin_edges = np.arange(0, max_val + bin_width, bin_width)
+    bin_centers = bin_edges[:-1] + (bin_width / 2)
+    hist_counts = []
+    for line_data in agg_data:
+        if len(line_data) == 0:
+            counts = np.zeros_like(bin_centers)
+        else:
+            counts, _ = np.histogram(line_data, bins=bin_edges)
+        hist_counts.append(counts)
+    return bin_centers, hist_counts
+
+
+
+# ----------------------------------------
+# ------IMPORT SPECIFIC PARTICLES---------
+# ----------------------------------------
+
+def import_part_types(part_file):
+    part_types = []
+    with open(part_file) as f:
+        for line in f:
+            part_types.append(int(line))
+    return part_types
+
+def import_traj_part_type(target_part_type, part_file, traj_file, frame_start = 1, frame_end = 100):
+    part_types = import_part_types(part_file)
+    n_particles = len(part_types)
+    selected_indices = []
+    for p, p_type in enumerate(part_types):
+        if p_type == target_part_type:
+            selected_indices.append(p)
+    traj_part_type = []
+    
+    with open(traj_file) as f:
+        while True:
+            header = f.readline()
+            if not header:
+                break # End of file!
+            frame, total_frames = map(int, header.strip().split(','))
+            if frame > frame_end:
+                break # Stop reading after end of specified frames range
+            if frame < frame_start:
+                for _ in range(n_particles):
+                    f.readline() # Simply skip and not parse the lines with frame < frame_start
+                continue
+            frame_coords = []
+            for i in range(n_particles):
+                line = f.readline()
+                if i in selected_indices:
+                    coords = tuple(map(float, line.split(',')))
+                    frame_coords.append(coords)
+            traj_part_type.append(frame_coords)
+        traj_part_type = np.array(traj_part_type)
+    return traj_part_type
+
+
+
+# ----------------------------------------
 # -------------TRANSITIONS----------------
 # ----------------------------------------
 
@@ -75,6 +173,8 @@ def sum_ass_diss_matrices(matrices):
                 except:
                     pass
     return sum_matrix
+
+
 
 # ----------------------------------------
 # ----------CONDENSATE BUILD--------------
